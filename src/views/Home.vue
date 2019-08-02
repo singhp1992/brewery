4f0cfa54c100a5f75e49bfcb6e480a23027c53b3
Preeti <singhp1992@gmail.com>
2019-08-02 16:23:35 +0200

    helloworld working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,33 @@
 <template>
   <div id="home">
     <h1> All Breweries </h1>
-    <ul>
-      <li
-        v-for="post in posts"
-        v-bind:key="post.id"
-        v-bind:title="post.title"
-      ></li>
-    </ul>
+    <HelloWorld msg="work"></HelloWorld>
   </div>
 </template>
 
 <script>
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default {
   name: 'home',
-  data () {
-    return {
-      li: [
-      { id: 1, 
-        title: 'Free State Brewery'
-      },
-      { id: 2, 
-        title: 'Avondale Brewing Co' 
-      },
-      { id: 3, 
-        title: 'DogFish'
-      }
-    ]
-    }
+  components: {
+    HelloWorld,
   }
+  // data () {
+  //   return {
+  //     li: [
+  //     { id: 1, 
+  //       title: 'Free State Brewery'
+  //     },
+  //     { id: 2, 
+  //       title: 'Avondale Brewing Co' 
+  //     },
+  //     { id: 3, 
+  //       title: 'DogFish'
+  //     }
+  //   ]
+  //   }
+  // }
 }
 
 
