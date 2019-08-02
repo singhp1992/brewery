c23f1113373b4907ba50aba0c85ecb7e5eaaea10
Preeti <singhp1992@gmail.com>
2019-08-02 14:32:01 +0200

    added routers

--- src/router.js
+++ src/router.js
@@ -1,23 +1,39 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'landing',
+      component: () => import('./views/Landing.vue'),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/welcome',
+      name: 'landing',
+      component: () => import('./views/Landing.vue'),
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: () => import('./views/Home.vue'),
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      component: () => import('./views/Categories.vue'),
+    },
+    {
+      path: '/random',
+      name: 'random',
+      component: () => import('./views/Random.vue'),
+    },
+    {
+      path: '/github',
+      beforeEnter() {
+        location.href = 'https://github.com/singhp1992'
+      }
     }
-  ]
-})
+  ],
+});
