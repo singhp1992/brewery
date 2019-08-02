7c6fc5b0350d7e4cc4c6529d0b2ce11cadfd1eac
Preeti <singhp1992@gmail.com>
2019-08-02 16:41:51 +0200

    redesigning all breweries

--- src/components/AllBrews.vue
+++ src/components/AllBrews.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <p>{{ msg }}</p>
   </div>
 </template>
 
@@ -16,18 +16,5 @@ export default {
 </script>
 
 <style>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
