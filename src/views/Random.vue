b228e391660709b51e23371c8886eb914755cc87
Preeti <singhp1992@gmail.com>
2019-08-07 14:30:19 +0200

    margin top for button and random info

--- src/views/Random.vue
+++ src/views/Random.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="random">
     <h1>Random</h1>
-
     <div class="random-info">
         <p>Title: {{ info.name }}</p>
         <p>Type: {{ info.brewery_type }}</p>
@@ -9,7 +8,6 @@
         <p>Contact: +1 {{ info.phone }}</p>
         <p>Website: {{ info.website_url }}</p>
     </div>
-
     <div class="random-button">
         <button>Another One!</button>
     </div>
@@ -46,8 +44,13 @@ export default {
   margin-top: 100px;
 }
 
+.random-info {
+  margin-top: 50px;
+}
+
 .random-button {
     text-align: center;
+    margin-top: 100px;
 }
 
 button {
@@ -58,6 +61,7 @@ button {
     font-size: 16px;
     -webkit-box-shadow: 0 5px 5px rgba(0, 0, 0, 0.07);
     cursor: pointer;
+    font-family: 'Roboto Mono', monospace, sans-serif;
 }
 
 </style>
