2b602ef8eac6bc563ff71842120edbc97184f0f5
Preeti <singhp1992@gmail.com>
2019-08-07 11:36:16 +0200

    added export default structure

--- src/views/Random.vue
+++ src/views/Random.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="random">
     <h1>Random</h1>
-    <!-- <Title msg="Random Brewery"/> -->
   </div>
 </template>
 
 <script>
 
+// export default {
+//     name: 'Random'
+// }
+
+import axios from 'axios'
+
 export default {
-    name: 'Random'
+  name: 'Random',
+  data() {
+    return {
+    }
+  },
+  methods: {
+
+  },
+  mounted() {
+    axios
+        .get('https://api.openbrewerydb.org/breweries?page=1&per_page=1')
+        .then(response => {
+            this.brewerys = response.data
+        })
+  },
+  computed: {
+
+  },
 }
+
 
 </script>
 
