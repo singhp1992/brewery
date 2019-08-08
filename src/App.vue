8fa63f6325dc9c5199bdb3739dbf24d8a3b1467e
Preeti <singhp1992@gmail.com>
2019-08-08 18:11:46 +0200

    adding footer vue

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,6 @@
   <div id="app">
     <Nav />
     <router-view/>
-    <div id="footer">
-      <router-link to="/home">Home</router-link> |
-      <router-link to="/categories">Categories</router-link> | 
-      <router-link to="/random">Random</router-link> |
-      <router-link to="/github" target="_blank">Preeti Singh</router-link>
-    </div>
   </div>
 </template>
 
---
+++ src/views/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <div id="footer">
+      <router-link to="/home">Home</router-link> |
+      <router-link to="/categories">Categories</router-link> | 
+      <router-link to="/random">Random</router-link> |
+      <router-link to="/github" target="_blank">Preeti Singh</router-link>
+    </div>
+</template>
