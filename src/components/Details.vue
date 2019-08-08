0d6efac723a2fbfb306a412b2eacd03c4ca642fa
Preeti <singhp1992@gmail.com>
2019-08-08 16:38:57 +0200

    setting up details page

---
+++ src/components/Details.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="single-blog"> 
+        <h1></h1>
+        <article> </article>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            id: null,
+            blog: {}
+        }
+    }
+}
+</script>
