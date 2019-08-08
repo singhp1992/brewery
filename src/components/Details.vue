bc3857c10f8999a9efcc9373d105258c2348e344
Preeti <singhp1992@gmail.com>
2019-08-08 16:42:56 +0200

    importing axios to details page

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,17 +1,29 @@
 <template>
-    <div id="single-blog"> 
+    <div id="details-page"> 
         <h1></h1>
-        <article> </article>
+        <p> </p>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     data() {
         return {
             id: this.$route.params.id,
-            blog: {}
+            brewerys: []
         }
-    }
+    },
+    mounted() {
+        axios
+        .get('https://api.openbrewerydb.org/breweries?page=1&per_page=10')
+        .then(response => {
+            this.brewerys = response.data
+        })
+  },
 }
 </script>
+
+<style>
+</style>
