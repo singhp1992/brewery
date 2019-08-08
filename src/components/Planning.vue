00f4c4ad1cc6c83a2bc5cd3762d35548d2d99879
Preeti <singhp1992@gmail.com>
2019-08-08 13:52:24 +0200

    creating planning component

---
+++ src/components/Planning.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="brewpub">
+    <ul class="brewpub-list"> 
+        <h3>Brewpub</h3>
+        <li v-bind:key="item.id" v-for="item in brewpubList"> 
+            <p>{{ item }}</p>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: "Brewpub",
+    props: [
+        "brewerys",
+        "brewpubList"
+    ],
+}
+
+
+</script>
+
+<style>
+
+#brewpub {
+    width: 45%;
+    padding-left: 30px;
+}
+
+.brewpub-list {
+  list-style-type: none;
+  padding-left: 0;
+  text-align: left;
+}
+
+</style>
