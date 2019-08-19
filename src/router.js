96eb97b319fdc312fbd8410afc2b81c8b4717e0e
Preeti <singhp1992@gmail.com>
2019-08-19 21:09:38 +0200

    adding more comments

--- src/router.js
+++ src/router.js
@@ -33,6 +33,7 @@ export default new Router({
     {
       path: '/github',
       beforeEnter() {
+        // directed towards my personal github
         location.href = 'https://github.com/singhp1992'
       }
     },
