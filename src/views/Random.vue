f0efc2a1dad61cab6b9a1716979ddc5a4a645e29
Preeti <singhp1992@gmail.com>
2019-08-19 21:05:54 +0200

    adding smaller font size on website on random

--- src/views/Random.vue
+++ src/views/Random.vue
@@ -62,7 +62,7 @@ export default {
   /* grid css */
   display: grid;
   max-width: 100%;
-  grid-template-columns: repeat(auto-fit, 350px);
+  grid-template-columns: repeat(auto-fit, 300px);
   justify-content: center;
 }
 
@@ -81,6 +81,7 @@ export default {
 
 .random-url {
   color: blue;
+  font-size: 12px;
 }
 
 .random-url:visited {
@@ -89,7 +90,7 @@ export default {
 
 .random-button {
   text-align: center;
-  margin-top: 40px;
+  margin-top: 20px;
 }
 
 button {
@@ -107,14 +108,10 @@ button {
 .card {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
-  width: 350px;
+  width: 300px;
   height: 400px;
   background-color: white;
   border-radius: 25px;
-}
-
-.card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
 }
 </style>
 
