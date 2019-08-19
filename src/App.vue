e9f6bff3d0dc50f1a8c5ebc1a79fb2a46daacd9d
Preeti <singhp1992@gmail.com>
2019-08-19 16:07:44 +0200

    cleaning up code

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,4 @@ export default {
 
 // improve design
 // get rid of blank space - look into modern websites for inspo
-// add mobile mediaqueries
-//  This time there are invalid html tags within the ul tag - look into this on dom
 // add more comments throughout application
-// change recommendation logic (new array)
